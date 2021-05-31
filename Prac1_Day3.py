0fda10c2c6d4617faddfe2c1ddcba6bf3b793a9b
PARMAR HARSH <73270753+hpcoding@users.noreply.github.com>
2021-05-31 18:48:02 +0530

    Delete Prac1_Day3.py

--- Prac1_Day3.py
+++
@@ -1,7 +0,0 @@
-n1 = int(input('Enter a number : '))
-n2 = int(input('Enter a number : '))
-n3 = int(input('Enter a number : '))
-n4 = int(input('Enter a number : '))
-n5 = int(input('Enter a number : '))
-
-print('The Average of 5 numbers is : ' ,(n1+n2+n3+n4+n5)/5)
