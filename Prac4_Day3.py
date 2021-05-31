c5510588d33437ab5204a88282430c0a1f9d1e19
PARMAR HARSH <73270753+hpcoding@users.noreply.github.com>
2021-05-31 18:47:35 +0530

    Delete Prac4_Day3.py

--- Prac4_Day3.py
+++
@@ -1,7 +0,0 @@
-n1 = int(input('Enter The Number : '))
-if(n1>0):
-    print(n1,'The Number Is Positive.')
-elif(n1<0):
-    print(n1,'The Number Is Negative.')
-else:
-    print(n1,'The Number Is Zero')
