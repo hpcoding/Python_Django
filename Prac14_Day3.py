679c59d4186d8dce95b47f63e50185c0e9c988c1
PARMAR HARSH <73270753+hpcoding@users.noreply.github.com>
2021-05-31 18:48:21 +0530

    Delete Prac14_Day3.py

--- Prac14_Day3.py
+++
@@ -1,9 +0,0 @@
-a = int(input('Enter Value Of A :'))
-b = int(input('Enter Value Of B :'))
-
-print('Value Of A before Swapping :', a)
-print('Value Of B before Swapping :', b)
-a, b = b, a
-
-print('Value Of A After Swapping :', a)
-print('Value Of B After Swapping :', b)
