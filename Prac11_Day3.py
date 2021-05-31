71abfcb0a8cb18432469d70e25296a9b8e2bc24d
PARMAR HARSH <73270753+hpcoding@users.noreply.github.com>
2021-05-31 18:46:40 +0530

    Delete Prac11_Day3.py

--- Prac11_Day3.py
+++
@@ -1,7 +0,0 @@
-a = int(input('Enter Value Of A : '))
-if(a<0):
-     print(a,'Is A Negative Number.')
-elif(a>0):
-         print(a,'Is A Number Is Positive.')
-else:
-    print('The Number Is Zero')
