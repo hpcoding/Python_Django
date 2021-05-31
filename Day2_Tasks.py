1451ec0e221dd0399ec2f6475c9106839afae72f
PARMAR HARSH <73270753+hpcoding@users.noreply.github.com>
2021-05-31 13:26:17 +0530

    Add files via upload

---
+++ Day2_Tasks.py
@@ -0,0 +1,109 @@
+#Task1
+a=10
+b=10.5
+c='Harsh Parmar'
+print(a)
+print(b)
+print('My Name Is',c)
+
+#Task2
+
+name='Harsh'
+print('My name is',name)
+#assigning a new value
+name='Kakaroth'
+print('My Name Is',name)
+
+#Task3
+
+a,b,c=10,30,'Harsh'
+print(a)
+print('Value of b :',b)
+print('My Name Is',c)
+
+#Task4
+
+a=b=c=45
+print(a)
+print('Value of B is',b)
+print('Value of C Is',c)
+
+#Task5
+
+a=10
+print(a,'Is A Type Of Class',type(a))
+b=10.5
+print(b,'Is A Type Of Class',type(b))
+c=1+10j
+print(c,'Is A Type Of Class',type(c))
+
+#Task6
+
+name='Harsh Parmar'
+
+#print Full String
+print('Name is',name)
+
+#print First character of string
+print(name[0])
+
+#print character starting from 3rd to 5th
+print(name[2:5])
+
+#print character starting from 3rd character
+print(name[2:])
+
+#print String 2 times
+print(name+name)
+
+#Print String with added charcters
+print('Hello',name)
+
+#Task7
+
+list=[10,30,'Kakaroth']
+print(list)
+
+#Task8
+list=[10,20,30,'Harsh',40,'Goku',50]
+
+#list[2]
+print('lIST[2] :',list[2])
+
+#list[0:3]
+print('List[0:3] :',list[0:3])
+
+#list[4:]
+print('List[4:] :',list[4:])
+
+#task9
+
+tuple=[10,20,30,'Kakaroth']
+print(tuple)
+
+#Task10
+
+tuple=[10,20,30,'Harsh',50,'Kakaroth',70]
+
+#First value of tuple
+print('Tuple[0] : ',tuple[0])
+
+#3Rd to 5th Value in tuple
+print('Tuple[2:5] :',tuple[2:5])
+
+#4th Onwards Value in tuple
+print('Tuple[4:] :',tuple[4:])
+
+
+#Task11
+
+a={1:'Harsh',2:'Kakaroth','Vegeta':3}
+
+print(type(a))
+
+print('a[1] : ',a[1])
+
+print('a[2] :',a[2])
+
+print('a[Vegeta] :',a['Vegeta'])
+
